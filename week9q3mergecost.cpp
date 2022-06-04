1991df191d42dabb68a4d47d5d40a34056978e77
sdr999 <100745287+sdr999@users.noreply.github.com>
2022-06-04 21:28:21 +0530

    Create week9q3mergecost.cpp

---
+++ week9q3mergecost.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    priority_queue<int, vector<int>, greater<int>> minheap;
+    for (int i = 0; i < n; i++) {
+        minheap.push(a[i]);
+    }
+    int ans = 0;
+    while (minheap.size() >1) {
+        int e1 = minheap.top();
+        minheap.pop();
+        int e2 = minheap.top();
+        minheap.pop();
+        ans += e1 + e2;
+        minheap.push(e1 + e2);
+    }
+    cout << ans;
+    return 0;
+}
